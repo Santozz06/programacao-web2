8f42cec6a59249ce56f4152c9b952e3885858c2b
Santozz06 <128476570+Santozz06@users.noreply.github.com>
2025-02-24 08:02:40 -0300

    Add files via upload

---
+++ aula2402.php
@@ -0,0 +1,18 @@
+<?php
+	//exeplo_var.php
+	//echo("ola");//
+	$nome = "Milena"; // string
+
+	$idade = 19; //int
+
+	$peso = 55.8; // float
+
+	$altura = 1.63; //float
+
+	$temCNH = false; //boolean
+
+	//printando as informações
+
+	echo("nome: ". $nome);
+
+?>
